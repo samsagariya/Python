c6f4229e871b17573b854a168c276791f86b1d42
samsagariya <32068522+samsagariya@users.noreply.github.com>
2020-05-25 17:33:02 +0530

    Update Somya.py

--- Somya.py
+++ Somya.py
@@ -1,13 +1,30 @@
+import pandas as pd
+import re
+df= pd.read_csv('1.csv', sep='|',error_bad_lines=False)
+dfn.reset_index()
+dfn.shape 
+
+eng =[]
+
 def isEnglish(s):
+
     try:
+
         s.encode(encoding='utf-8').decode('ascii')
+
     except UnicodeDecodeError:
+
         return False
+
     else:
+
         return True
 
-for idx,item in enumerate(df.title):
-    if not(isEnglish(item)) == True:
 
-        df.drop(idx, inplace=True)
-print(df)
+
+for idx,item in enumerate(dfn.title):
+
+    if isEnglish(item) == False:
+        eng.append(idx)
+               
+print(eng)
