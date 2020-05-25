b68e95335adfe86f9b033b8a7a975f3adc8e8ec4
samsagariya <32068522+samsagariya@users.noreply.github.com>
2020-05-25 18:10:56 +0530

    Update Somya.py

--- Somya.py
+++ Somya.py
@@ -28,3 +28,12 @@ for idx,item in enumerate(dfn.title):
         eng.append(idx)
                
 print(eng)
+
+def engprepo(line):
+    line=line.lower()
+    doc_spacy = en_nlp(line)
+    a = [token.lemma_ for token in doc_spacy]
+    my_stop=set(stopwords.words('english')+list(punctuation))
+    words=[w for w in a if not w in my_stop if len(w)>2]
+    return words
+
